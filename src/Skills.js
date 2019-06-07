82b6f007fc6c077ca8e1f4323ba5e40770639367
Darren Taylor <shout@darrenonthe.net>
2019-06-07 16:00:04 +0100

    Progress a little on the Skills page

--- src/Skills.js
+++ src/Skills.js
@@ -7,9 +7,121 @@ function Skills() {
       <PageTitle title="Skills" />
 
       <div class="pt-10 container mx-auto">
-        <p className="text">
-          I've worked with alot of different tech so far in my career. I'm not an expert at everything and likely never will be.
-        </p>
+
+        <div className="flex flex-wrap">
+          <div className="lg:w-1/4">
+            <div className="portfolio-card rounded shadow mr-4">
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">PHP</div>
+                <p className="text-gray-700 text-base">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
+                  perferendis eaque, exercitationem praesentium nihil.
+                </p>
+              </div>
+              <div className="px-6 py-4">
+              <span
+                className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#backend</span>
+                <span
+                  className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#language</span>
+              </div>
+            </div>
+
+          </div>
+          <div className="lg:w-1/4">
+            <div className="portfolio-card rounded shadow mr-4">
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">PHP</div>
+                <p className="text-gray-700 text-base">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
+                  perferendis eaque, exercitationem praesentium nihil.
+                </p>
+              </div>
+              <div className="px-6 py-4">
+              <span
+                className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#backend</span>
+                <span
+                  className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#language</span>
+              </div>
+            </div>
+
+          </div>
+          <div className="lg:w-1/4">
+            <div className="portfolio-card rounded shadow mr-4">
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">PHP</div>
+                <p className="text-gray-700 text-base">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
+                  perferendis eaque, exercitationem praesentium nihil.
+                </p>
+              </div>
+              <div className="px-6 py-4">
+              <span
+                className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#backend</span>
+                <span
+                  className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#language</span>
+              </div>
+            </div>
+
+          </div>
+          <div className="lg:w-1/4">
+            <div className="portfolio-card rounded shadow mr-4">
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">PHP</div>
+                <p className="text-gray-700 text-base">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
+                  perferendis eaque, exercitationem praesentium nihil.
+                </p>
+              </div>
+              <div className="px-6 py-4">
+              <span
+                className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#backend</span>
+                <span
+                  className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#language</span>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="lg:w-1/4">
+            <div className="portfolio-card rounded shadow mr-4">
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">PHP</div>
+                <p className="text-gray-700 text-base">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
+                  perferendis eaque, exercitationem praesentium nihil.
+                </p>
+              </div>
+              <div className="px-6 py-4">
+              <span
+                className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#backend</span>
+                <span
+                  className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#language</span>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="lg:w-1/4">
+            <div className="portfolio-card rounded shadow mr-4">
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">PHP</div>
+                <p className="text-gray-700 text-base">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
+                  perferendis eaque, exercitationem praesentium nihil.
+                </p>
+              </div>
+              <div className="px-6 py-4">
+              <span
+                className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#backend</span>
+                <span
+                  className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#language</span>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+
       </div>
     </div>
   )
